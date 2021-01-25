8ac83a3d86d21f2a1c6bd6e8549af6b07e426e84
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-01-25 11:25:20 +0100

    json_plist (first attempt)

---
+++ plistyamlplist_lib/json_plist.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""If this script is run directly, it takes an input file and an output file
+from the command lineThe input file must be in YAML format. The output file
+will be in PLIST format:
+
+yaml_plist.py <input-file> <output-file>
+
+The output file can be omitted, so long as the input file ends with .yaml.
+In this case, the name of the output file is
+taken from the input file, with .yaml removed from the end.
+For best results, the input file should therefore be named with
+"""
+
+import json
+import sys
+import os.path
+
+try:  # python 3
+    from plistlib import dumps as write_plist
+except ImportError:  # python 2
+    from plistlib import writePlistToString as write_plist
+
+
+def convert(data):
+    """Do the conversion."""
+    lines = write_plist(data).splitlines()
+    lines.append("")
+    return "\n".join(lines)
+
+
+def json_plist(in_path, out_path):
+    """Convert json to plist."""
+    try:
+        in_file = open(in_path, "r")
+    except IOError:
+        print("ERROR: {} not found".format(in_path))
+        return
+    try:
+        out_file = open(out_path, "w")
+    except IOError:
+        print("ERROR: could not create {} ".format(out_path))
+        return
+
+    input_data = json.load(in_file)
+    output = convert(input_data)
+
+    out_file.writelines(output)
+    print("Wrote to : {}\n".format(out_path))
+
+
+def main():
+    """Get the command line inputs if running this script directly."""
+    if len(sys.argv) < 2:
+        print("Usage: json_plist.py <input-file> <output-file>")
+        sys.exit(1)
+
+    in_path = sys.argv[1]
+    try:
+        sys.argv[2]
+    except Exception as e:
+        print(e)  # TODO - temp to determine correct exception
+        if in_path.endswith(".json"):
+            filename, _ = os.path.splitext(in_path)
+            out_path = filename
+        else:
+            print("Usage: json_plist.py <input-file> <output-file>")
+            sys.exit(1)
+    else:
+        out_path = sys.argv[2]
+
+    json_plist(in_path, out_path)
+
+
+if __name__ == "__main__":
+    main()
